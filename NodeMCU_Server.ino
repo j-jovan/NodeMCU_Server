e6f217e12f5bab53362c06777be082e97dd1e5e4
Jovan <abc.j.jovan@gmail.com>
2019-06-12 11:23:04 +0200

    Change IP address to static

--- NodeMCU_Server.ino
+++ NodeMCU_Server.ino
@@ -31,6 +31,12 @@ void setup() {
   Serial.println(ssid);
 
   WiFi.begin(ssid, password);
+  // Fiksiranje IP adrese
+  IPAddress ip(192, 168, 0, 200);
+  IPAddress gateway(192, 168, 0, 1);
+  IPAddress subnet(255, 255, 255, 0);
+  WiFi.config(ip, gateway, subnet);
+  WiFi.mode(WIFI_STA);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -68,79 +74,79 @@ void loop() {
   Serial.println(request);
   client.flush();
 
-  // Uporedi zahteb
+  // Uporedi zahtev
 
   int value13 = LOW;
-  if (request.indexOf("/LED13=ON") != -1)  {
+  if (request.indexOf("/PIN13=ON") != -1)  {
     digitalWrite(ledPin13, HIGH);
     value13 = HIGH;
   }
-  if (request.indexOf("/LED13=OFF") != -1)  {
+  if (request.indexOf("/PIN13=OFF") != -1)  {
     digitalWrite(ledPin13, LOW);
     value13 = LOW;
   }
   int value12 = LOW;
-  if (request.indexOf("/LED12=ON") != -1)  {
+  if (request.indexOf("/PIN12=ON") != -1)  {
     digitalWrite(ledPin12, HIGH);
     value12 = HIGH;
   }
-  if (request.indexOf("/LED12=OFF") != -1)  {
+  if (request.indexOf("/PIN12=OFF") != -1)  {
     digitalWrite(ledPin12, LOW);
     value12 = LOW;
   }
   int value14 = LOW;
-  if (request.indexOf("/LED14=ON") != -1)  {
+  if (request.indexOf("/PIN14=ON") != -1)  {
     digitalWrite(ledPin14, HIGH);
     value14 = HIGH;
   }
-  if (request.indexOf("/LED14=OFF") != -1)  {
+  if (request.indexOf("/PIN14=OFF") != -1)  {
     digitalWrite(ledPin14, LOW);
     value14 = LOW;
   }
   int value2 = LOW;
-  if (request.indexOf("/LED2=ON") != -1)  {
+  if (request.indexOf("/PIN2=ON") != -1)  {
     digitalWrite(ledPin2, HIGH);
     value2 = HIGH;
   }
-  if (request.indexOf("/LED2=OFF") != -1)  {
+  if (request.indexOf("/PIN2=OFF") != -1)  {
     digitalWrite(ledPin2, LOW);
     value2 = LOW;
   }
   int value0 = LOW;
-  if (request.indexOf("/LED0=ON") != -1)  {
+  if (request.indexOf("/PIN0=ON") != -1)  {
     digitalWrite(ledPin0, HIGH);
     value0 = HIGH;
   }
-  if (request.indexOf("/LED0=OFF") != -1)  {
+  if (request.indexOf("/PIN0=OFF") != -1)  {
     digitalWrite(ledPin0, LOW);
     value0 = LOW;
   }
 
   int value4 = LOW;
-  if (request.indexOf("/LED4=ON") != -1)  {
+  if (request.indexOf("/PIN4=ON") != -1)  {
     digitalWrite(ledPin4, HIGH);
     value4 = HIGH;
   }
-  if (request.indexOf("/LED4=OFF") != -1)  {
+  if (request.indexOf("/PIN4=OFF") != -1)  {
     digitalWrite(ledPin4, LOW);
     value4 = LOW;
   }
 
   int value5 = LOW;
-  if (request.indexOf("/LED5=ON") != -1)  {
+  if (request.indexOf("/PIN5=ON") != -1)  {
     digitalWrite(ledPin5, HIGH);
     value5 = HIGH;
   }
-  if (request.indexOf("/LED5=OFF") != -1)  {
+  if (request.indexOf("/PIN5=OFF") != -1)  {
     digitalWrite(ledPin5, LOW);
     value5 = LOW;
   }
   int value16 = LOW;
-  if (request.indexOf("/LED16=ON") != -1)  {
+  if (request.indexOf("/PIN16=ON") != -1)  {
     digitalWrite(ledPin16, HIGH);
     value16 = HIGH;
   }
-  if (request.indexOf("/LED16=OFF") != -1)  {
+  if (request.indexOf("/PIN16=OFF") != -1)  {
     digitalWrite(ledPin16, LOW);
     value16 = LOW;
   }
@@ -179,10 +185,10 @@ void loop() {
     }
   }
 
-  // Return the response
+  // Vrati odgovor
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
-  client.println(""); //  do not forget this one
+  client.println(""); 
   client.println("<!DOCTYPE HTML>");
   client.println("<html>");
 
@@ -194,10 +200,10 @@ void loop() {
     client.print("Off");
   }
   client.println("<br>");
-  client.println("<a href=\"/LED13=ON\"\"><button>Turn On </button></a>");
-  client.println("<a href=\"/LED13=OFF\"\"><button>Turn Off </button></a><br />");
+  client.println("<a href=\"/PIN13=ON\"\"><button>Turn On </button></a>");
+  client.println("<a href=\"/PIN13=OFF\"\"><button>Turn Off </button></a><br />");
   client.println("<br>");
-  //j
+
   client.print("Led pin 12 is now: ");
 
   if (value12 == HIGH) {
@@ -206,10 +212,9 @@ void loop() {
     client.print("Off");
   }
   client.println("<br>");
-  client.println("<a href=\"/LED12=ON\"\"><button>Turn On </button></a>");
-  client.println("<a href=\"/LED12=OFF\"\"><button>Turn Off </button></a><br />");
+  client.println("<a href=\"/PIN12=ON\"\"><button>Turn On </button></a>");
+  client.println("<a href=\"/PIN12=OFF\"\"><button>Turn Off </button></a><br />");
   client.println("<br>");
-  //j
 
   client.print("Led pin 14 is now: ");
 
@@ -219,8 +224,8 @@ void loop() {
     client.print("Off");
   }
   client.println("<br>");
-  client.println("<a href=\"/LED14=ON\"\"><button>Turn On </button></a>");
-  client.println("<a href=\"/LED14=OFF\"\"><button>Turn Off </button></a><br />");
+  client.println("<a href=\"/PIN14=ON\"\"><button>Turn On </button></a>");
+  client.println("<a href=\"/PIN14=OFF\"\"><button>Turn Off </button></a><br />");
   client.println("<br>");
 
   client.print("Led pin 2 is now: ");
@@ -231,8 +236,8 @@ void loop() {
     client.print("Off");
   }
   client.println("<br>");
-  client.println("<a href=\"/LED2=ON\"\"><button>Turn On </button></a>");
-  client.println("<a href=\"/LED2=OFF\"\"><button>Turn Off </button></a><br />");
+  client.println("<a href=\"/PIN2=ON\"\"><button>Turn On </button></a>");
+  client.println("<a href=\"/PIN2=OFF\"\"><button>Turn Off </button></a><br />");
   client.println("<br>");
 
   client.print("Led pin 0 is now: ");
@@ -243,8 +248,8 @@ void loop() {
     client.print("Off");
   }
   client.println("<br>");
-  client.println("<a href=\"/LED0=ON\"\"><button>Turn On </button></a>");
-  client.println("<a href=\"/LED0=OFF\"\"><button>Turn Off </button></a><br />");
+  client.println("<a href=\"/PIN0=ON\"\"><button>Turn On </button></a>");
+  client.println("<a href=\"/PIN0=OFF\"\"><button>Turn Off </button></a><br />");
   client.println("<br>");
 
   client.print("Led pin 4 is now: ");
@@ -255,8 +260,8 @@ void loop() {
     client.print("Off");
   }
   client.println("<br>");
-  client.println("<a href=\"/LED4=ON\"\"><button>Turn On </button></a>");
-  client.println("<a href=\"/LED4=OFF\"\"><button>Turn Off </button></a><br />");
+  client.println("<a href=\"/PIN4=ON\"\"><button>Turn On </button></a>");
+  client.println("<a href=\"/PIN4=OFF\"\"><button>Turn Off </button></a><br />");
   client.println("<br>");
 
   client.print("Led pin 5 is now: ");
@@ -267,8 +272,8 @@ void loop() {
     client.print("Off");
   }
   client.println("<br>");
-  client.println("<a href=\"/LED5=ON\"\"><button>Turn On </button></a>");
-  client.println("<a href=\"/LED5=OFF\"\"><button>Turn Off </button></a><br />");
+  client.println("<a href=\"/PIN5=ON\"\"><button>Turn On </button></a>");
+  client.println("<a href=\"/PIN5=OFF\"\"><button>Turn Off </button></a><br />");
   client.println("<br>");
 
   client.print("Led pin 16 is now: ");
@@ -279,8 +284,8 @@ void loop() {
     client.print("Off");
   }
   client.println("<br>");
-  client.println("<a href=\"/LED16=ON\"\"><button>Turn On </button></a>");
-  client.println("<a href=\"/LED16=OFF\"\"><button>Turn Off </button></a><br />");
+  client.println("<a href=\"/PIN16=ON\"\"><button>Turn On </button></a>");
+  client.println("<a href=\"/PIN16=OFF\"\"><button>Turn Off </button></a><br />");
   client.println("<br>");
   client.println("<a href=\"/REDNO1\"\"><button>Diode Redno 1 </button></a>");
   client.println("<a href=\"/REDNO2\"\"><button>Diode Redno 2 </button></a>");
